e497e217f4c396f66b0500ba59eb8161e956697c
codejoes <josephscodes@gmail.com>
2023-02-08 22:42:25 -0500

    adding working schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,13 @@
--- INSERT INTO department (title)
--- VALUES ('Information Technology');
+USE employees_db;
 
--- INSERT INTO role (title)
--- VALUES ('Team Lead')
+INSERT INTO departments (title) VALUES
+	('Management'),
+    ('Information Technology');
+INSERT INTO roles (title, salary, department_id) VALUES
+	('General Manager', 110000, 1),
+    ('Manager', 65000, 1),
+    ('Software Developer', 60000, 2);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
+	('John', 'Doe', 1, NULL),
+    ('Jane', 'Doe', 2, 1),
+    ('Jack', 'Trout', 3, NULL);
